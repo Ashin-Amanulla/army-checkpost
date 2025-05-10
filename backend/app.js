ef340f17af06d1fcb7506c5a2834877fb9bcaa18
xyvintech <admin@xyvin.com>
2025-05-10 12:58:35 +0530

    env

--- backend/app.js
+++ backend/app.js
@@ -35,8 +35,8 @@ app.use(cors({
 
   app.use(morgan('dev'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Swagger Documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
